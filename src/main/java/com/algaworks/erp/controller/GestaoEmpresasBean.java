daa45414de7196d175e50ec45c98ac4cecab93c6
Fabricio Macedo <fabriciusiqueira@gmail.com>
2022-05-18 09:37:23 -0300

    Criando o Managed Bean

--- src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -2,27 +2,33 @@ package com.algaworks.erp.controller;
 
 import java.io.Serializable;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import com.algaworks.erp.model.Empresa;
+import com.algaworks.erp.model.TipoEmpresa;
+
 @Named
-//@RequestScoped//Ciclo de vida: Instancia criada quando requisição é criada e encerrada quando requisição é encerrada
-//@ViewScoped//Ciclo de vida: Instancia criada quando é carregada a página (View) e termina quando a sessão termina ou alguma outra navegação dentro de um componente
-//@SessionScoped//Ciclo de vida: Instancia criada quando a sessão é criada, e é encerrada quando a sessão é encerrada
-@ApplicationScoped//Ciclo de vida: Instancia é criada quando a aplicação sobe, e permanece por toda a aplicação
+@ViewScoped
 public class GestaoEmpresasBean implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
-    private static Integer NUMERO = 0;
+    private Empresa empresa = new Empresa();
     
-    public GestaoEmpresasBean() {
-        NUMERO++;
+    public void salvar() {
+    	System.out.println("Razão Social: " + empresa.getRazaoSocial() + 
+    			"\nNome Fantasia: " + empresa.getNomeFantasia() + 
+    			"\nTipo: " + empresa.getTipo());
     }
     
-    public Integer getNumero() {
-        return NUMERO;
+    public Empresa getEmpresa() {
+		return empresa;
+	}
+    
+    public TipoEmpresa[] getTiposEmpresa() {
+    	return TipoEmpresa.values();
     }
+    
+    
 }
